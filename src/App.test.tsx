cfbdf1c0ef943daa9fb0b1990813e999dd0a12df
Craig Hughes <hghscraig@gmail.com>
2025-01-21 22:07:40 +0000

    chore: add FileEntry test & FileExplorer test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
-import { test, expect } from "vitest";
+import { test, expect, vi } from "vitest";
 import { render, screen } from '@testing-library/react'
 import App from "./App";
 
 test('loads and displays file entries', async () => {
     render(<App />);
 
-    const fileEntries = await screen.getAllByTestId("file-entry");
+    expect(await screen.findByTestId("loading")).toBeTruthy();
+
+    const fileEntries = await vi.waitFor(async () => await screen.getAllByTestId("file-entry"));
     expect(fileEntries.length).toBe(5);
 })
